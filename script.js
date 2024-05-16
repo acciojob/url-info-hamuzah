2c74653e2266ad8f1e16ad1af1d299778ccb2687
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-05-16 12:49:57 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //your JS code here. If required.
+// Step 1: Access the current URL using window.location.href
+const currentURL = window.location.href;
 
-let curr = window.location.href;
+// Step 2: Count the number of characters in the URL using the length property
+const urlLength = currentURL.length;
 
-let currlen = curr.length;
-
-alert(currlen);
+// Step 3: Print the length of the URL using alert()
+alert("The length of the URL is: " + urlLength);
